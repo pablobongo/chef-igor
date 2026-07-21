2eecc8a754a7916ac5a51e935997e02bcece8dda
Igor Rigon <igor.lavoro.pb@gmail.com>
2026-07-21 22:27:42 +0200

    Debug temporaneo: log dettagliato in fase di avvio
    Log diagnostici per capire perché init() non produce effetti sul
    dominio GitHub Pages nonostante nessun errore visibile finora.
    Da rimuovere una volta trovata la causa.

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
    Regola critica: NON intercettare mai script.google.com
 ============================================================ */
 
-const CACHE_NAME = 'chef-igor-v1.0.2';
+const CACHE_NAME = 'chef-igor-v1.0.3';
 
 // Asset da mettere in cache all'installazione
 const ASSET_DA_CACHARE = [
